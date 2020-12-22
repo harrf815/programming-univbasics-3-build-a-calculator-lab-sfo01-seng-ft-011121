96f3fa23bb6b33adfce588cfbb4bdff2a91d800f
Harrf Akbar <harrf_akbar@yahoo.com>
2020-12-22 23:26:27 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,21 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  puts sum
 end
 
 def subtraction(num1, num2)
-
+  total = num1 - num2
+  puts total
 end
 
 def division(num1, num2)
-
+  total = num1 / num2
+  puts total
 end
 
 def multiplication(num1, num2)
-
+  total = num1 * num2 
+  puts total
 end
 
 def modulo(num1, num2)
@@ -19,5 +23,5 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-
+  total = num **2
 end
